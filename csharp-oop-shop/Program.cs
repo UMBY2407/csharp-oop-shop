bf49daa5af7801a1760ae53404ac9e607e029bad
UMBY2407 <umby2407@gmail.com>
2023-04-18 16:30:27 +0200

    added getter, setters, creaProdotto and printProduct

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_oop_shop;
+
+bool productFlag = false;
+Console.Write("Vuoi inserire un prodotto? (si/no)");
+string productFlagString = Console.ReadLine();
+if(productFlagString == "NO" || productFlagString == "no" || productFlagString == "No")
+{
+	productFlag = true;
+}
+while (!productFlag)
+{
+	Prodotto prodotto = new Prodotto();
+
+	prodotto.creaProdotto();
+
+	Console.Write("Vuoi stampare il prodotto appena creato? ");
+	string printProductString = Console.ReadLine();
+	if(printProductString == "SI" || printProductString == "si" || productFlagString == "Si")
+	{
+		prodotto.printProduct();
+	}
+
+	Console.Write("Vuoi inserire un altro prodotto? (si/no))");
+	productFlagString = Console.ReadLine();
+	if (productFlagString == "NO" || productFlagString == "no" || productFlagString == "No")
+	{
+		productFlag = true;
+	}
+}
